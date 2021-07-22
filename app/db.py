56ecf770c84207a67f76b85e9f1f62544f267df7
tenra <tetenchi@yahoo.co.jp>
2021-07-22 14:36:21 +0900

    env.dev

--- app/db.py
+++ app/db.py
@@ -3,27 +3,26 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 
 # 接続したいDBの基本情報を設定
-user_name = "user"
-password = "password"
-host = "db"  # docker-composeで定義したMySQLのサービス名
-database_name = "sample_db"
+user_name = os.environ.get("MYSQL_USER")
+password = os.environ.get("MYSQL_PASSWORD")
+host = os.environ.get("MYSQL_HOST")
+database_name = os.environ.get("MYSQL_DATABASE")
 
-DATABASE = 'mysql://%s:%s@%s/%s?charset=utf8' % (
-    user_name,
-    password,
-    host,
-    database_name,
-)
+DATABASE = 'mysql://%s:%s@%s/%s?charset=utf8' % (user_name, password, host, database_name)
 
 # DBとの接続
-ENGINE = create_engine(
-    DATABASE,
-    encoding="utf-8",
-    echo=True
-)
+ENGINE = create_engine(DATABASE, encoding="utf-8", echo=True)
 
 # Sessionの作成
 session = scoped_session(
